25e28d4819623ff39b9c0493acc836660440df5e
aaronjimenez97 <aaronjimenez97@csu.fullerton.edu>
2018-12-12 15:00:52 -0800

    Small update for the checkout features. Not running quite yet. Need to get the manifest file to include date

--- VCSProject/VCSProject/CreateRepository.h
+++ VCSProject/VCSProject/CreateRepository.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <experimental/filesystem>
+// #include "date.h"
 
 namespace fs = std::experimental::filesystem;
 
@@ -11,6 +12,7 @@ namespace fs = std::experimental::filesystem;
 std::string artifactID(fs::path);
 void createRepository(std::string, std::string);
 
+//void checkout(std::string,std::string,std::string);	//Addition to the File
 
 std::string artifactID(fs::path directoryPath) {
 	std::string artifactFile;
@@ -53,13 +55,30 @@ std::string artifactID(fs::path directoryPath) {
 void createRepository(std::string existingDirectory, std::string newDirectory) {
 	fs::path currentPath = existingDirectory;
 	std::cout << "current path: " << currentPath << std::endl;
-
+	std::ofstream maniFile_;
+	maniFile_.open(currentPath+".txt");		//Creates the manifest file for the repo of the given path
 	fs::copy(existingDirectory, newDirectory, fs::copy_options::recursive);
 	for (auto &it : fs::recursive_directory_iterator(newDirectory)) {
 		if (fs::is_regular_file(it.path())) {
+		//	std::string timeStamp = date::format("%F %T",std::chrono::system_clock::now());	Trying to get the time stamp in the manifest file working
 			std::string newArtifact = artifactID(it.path());
-
+			//maniFile_ << it.path() << "\t"<<newArtifact << timeStamp <<std::endl;
+			maniFile_ << it.path() << "\t"<<newArtifact << std::endl;
 			fs::rename(it.path(), fs::path(it.path()).replace_filename(newArtifact));
 		}
 	}
+	maniFile_.close();
+}
+//manfileExists()
+
+checkout(std::string repoToCheckout, std::string directoryTarget, std::string manFile)  //"checks out repo (copies the repository)"
+{
+   fs::path repoPath = repoToCheckout;
+	 if()
+	 std:: cout <<"Checking out: current path: "<< repoToCheckout<< std::endl;
+	 std::
+ 	 if(fs::exists )
+ 	 fs::copy(repoToCheckout, newDirectory, fs::copy_options::recur)
+   std::ifstream directoryFile;
+	 directoryFile.open(directoryPath);
 }
